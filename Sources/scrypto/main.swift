ef2fcfe09e7f30bb05da36a902ad2a0e40e12c68
Tom Rochat <tom.roch4t@gmail.com>
2020-08-21 13:55:34 -1000

    add argument parser dependency

--- Sources/scrypto/main.swift
+++ Sources/scrypto/main.swift
@@ -1,1 +1,13 @@
-print("Hello, world!")
+import Foundation
+import ArgumentParser
+
+struct Scrypto: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        commandName: "scrypto",
+        abstract: "a small tool to play with rsa",
+        version: "0.0.1",
+        subcommands: []
+    )
+}
+
+Scrypto.main()
