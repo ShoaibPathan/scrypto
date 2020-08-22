e2cd686ab92f449c3322e662f106e3db2d24ab80
Tom Rochat <tom.roch4t@gmail.com>
2020-08-21 14:42:53 -1000

    add cryptorrsa dep

--- Package.swift
+++ Package.swift
@@ -7,12 +7,14 @@ let package = Package(
     name: "scrypto",
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
+        .package(name: "CryptorRSA", url: "https://github.com/IBM-Swift/BlueRSA", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "scrypto",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "CryptorRSA", package: "CryptorRSA")
             ]),
         .testTarget(
             name: "scryptoTests",
